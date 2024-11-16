bb0cef6c13157f81ec93366d6677ee09b98148e5
GitHub Action <action@github.com>
2024-11-16 00:39:24 +0000

    Publish template output for Tailwind

---
+++ BlazoriseBugReportTailwind_241116.Client/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportTailwind_241116.Client.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ BlazoriseBugReportTailwind_241116.Client/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportTailwind_241116.Client.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
