370f08e34d0e98cb02e1cb99583104b01b15017f
GitHub Action <action@github.com>
2024-12-11 00:41:53 +0000

    Publish template output for Tailwind

---
+++ BlazoriseBugReportTailwind_241211.Client/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportTailwind_241211.Client.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
